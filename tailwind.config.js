c849560d362b5fcf32dd1ee2d99b6e64036a2332
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-09 15:32:00 +0100

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js,images}"],
+  content: ["./public/*.{html,js,images}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': '"Roboto", sans-serif',
+      }, 
+      colors: {
+        'gray-400': '#b4b4b4;',
+        'gray-750': '#2f2f2f',
+      }
+    },
   },
   plugins: [],
 }
